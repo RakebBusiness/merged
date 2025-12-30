def8d46787c81307f0d0f52ab10fe0ed8a70bd26
RakebBusiness <rakebbusiness@gmail.com>
2025-12-30 16:47:15 +0100

    Updated database_schema.sql

--- back/database_schema.sql
+++ back/database_schema.sql
@@ -295,3 +295,24 @@ WHERE "enrolledAt" IS NULL;
 -- Create indexes for performance
 CREATE INDEX IF NOT EXISTS idx_etudiant_cours_enrolled ON "ETUDIANT_COURS"("enrolledAt");
 CREATE INDEX IF NOT EXISTS idx_etudiant_cours_finished ON "ETUDIANT_COURS"("finishedAt");
+DO $$
+DECLARE
+    max_id INT;
+BEGIN
+    -- Get the maximum current idCours value
+    SELECT COALESCE(MAX("idCours"), 0) INTO max_id FROM "COURS";
+
+    -- Create sequence if it doesn't exist
+    IF NOT EXISTS (SELECT 1 FROM pg_sequences WHERE schemaname = 'public' AND sequencename = 'cours_idcours_seq') THEN
+        EXECUTE format('CREATE SEQUENCE cours_idcours_seq START WITH %s', max_id + 1);
+    ELSE
+        -- Reset sequence to max value + 1
+        EXECUTE format('ALTER SEQUENCE cours_idcours_seq RESTART WITH %s', max_id + 1);
+    END IF;
+
+    -- Set the default value to use the sequence
+    ALTER TABLE "COURS" ALTER COLUMN "idCours" SET DEFAULT nextval('cours_idcours_seq');
+
+    -- Make sure the sequence is owned by the column
+    ALTER SEQUENCE cours_idcours_seq OWNED BY "COURS"."idCours";
+END $$;
